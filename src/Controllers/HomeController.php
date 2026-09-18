44a0afb02f4003d5527fd335f4e209821e937c21
Mark Kiprotich Mugun <mugunmark81@gmail.com>
2026-09-18 21:33:52 +0300

    Update framework version in controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -10,7 +10,7 @@ final class HomeController
 {
     public function index(): Response
     {
-        return new Response('Welcome to Taydence Framework v0.2');
+        return new Response('Welcome to Taydence Framework v0.4');
     }
 
     public function hello(): Response
