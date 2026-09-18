dda742e0c9b581bc72f6c58d6db08897f8199c27
Mark Kiprotich Mugun <mugunmark81@gmail.com>
2026-09-18 21:30:26 +0300

    feat: build Taydence Framework v0.2 - public/index.php

--- public/index.php
+++ public/index.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Taydence\Application;
+use Taydence\Controllers\HomeController;
 use Taydence\Http\Request;
+use Taydence\Http\Response;
 
 $app = new Application();
 
-$app->get('/', fn () => 'Hello from Taydence Framework!');
+$app->get('/', [HomeController::class, 'index']);
 $app->get('/about', fn () => 'A tiny PHP framework built from scratch.');
 $app->get('/hello', function (Request $request) {
     return 'Hello ' . ($request->query('name') ?? 'there');
 });
+$app->get('/hello/{name}', fn (Request $request, string $name) => 'Hello ' . $name);
+$app->get('/users/{id}', fn (Request $request, string $id) => Response::json([
+    'id' => $id,
+]));
 
 $app->run();
