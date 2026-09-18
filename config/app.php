baae114601be26b9258aa94b77916849a5bbca15
Mark Kiprotich Mugun <mugunmark81@gmail.com>
2026-09-18 21:51:39 +0300

    Use environment values in application config

--- config/app.php
+++ config/app.php
@@ -3,7 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'name' => 'Taydence Framework',
-    'env' => 'local',
-    'debug' => true,
+    'name' => env('APP_NAME', 'Taydence Framework'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'database' => [
+        'host' => env('DB_HOST', 'localhost'),
+        'port' => env('DB_PORT', 3306),
+        'database' => env('DB_DATABASE', ''),
+        'username' => env('DB_USERNAME', ''),
+        'password' => env('DB_PASSWORD', ''),
+    ],
 ];
