e18c961963f3c8636ea072f45ddf2bc0b07fdca0
Mark Kiprotich Mugun <mugunmark81@gmail.com>
2026-09-18 21:30:11 +0300

    feat: build Taydence Framework v0.2 - src/Http/Response.php

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -12,6 +12,15 @@ final class Response
         private readonly array $headers = ['Content-Type' => 'text/html; charset=UTF-8'],
     ) {}
 
+    public static function json(array $data, int $status = 200): self
+    {
+        return new self(
+            json_encode($data, JSON_THROW_ON_ERROR),
+            $status,
+            ['Content-Type' => 'application/json; charset=UTF-8']
+        );
+    }
+
     public function send(): void
     {
         http_response_code($this->status);
