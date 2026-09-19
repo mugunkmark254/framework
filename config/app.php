b23b1a3edd86cba4102f2f49c591ca6783826ee2
Mark Kiprotich Mugun <mugunmark81@gmail.com>
2026-09-19 07:59:21 +0300

    Configure database connection from environment

--- config/app.php
+++ config/app.php
@@ -7,10 +7,12 @@ return [
     'env' => env('APP_ENV', 'production'),
     'debug' => env('APP_DEBUG', false),
     'database' => [
+        'driver' => env('DB_CONNECTION', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
         'port' => env('DB_PORT', 3306),
         'database' => env('DB_DATABASE', ''),
         'username' => env('DB_USERNAME', ''),
         'password' => env('DB_PASSWORD', ''),
+        'charset' => env('DB_CHARSET', 'utf8mb4'),
     ],
 ];
